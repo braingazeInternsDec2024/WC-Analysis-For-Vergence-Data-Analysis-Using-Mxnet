33fb6183f650bac556f6b4016ecd917f74de0d0e
SandaruwanWGMA <sandaruwanwgma@gmail.com>
2025-02-05 08:54:20 +0530

    main.py edited

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-This script usese BrainGaze class wrapper to process 3 log files given by wctest software
+This script uses the BrainGaze class wrapper to process log files given by wctest software.
 @author: leono
 """
 
 import sys
 from BRAINGAZE_class import *
-
 from VergenceAnalysis import VergenceCalculator
 from vergence_plotter_induvidual import plot_all_csv_in_dir
 
-
 def main(path2logs):
-
     # Print a start message to confirm the script is running
-    print("Starting computations...")
+    print(f"Starting computations for path: {path2logs}")
 
-    data = path2logs
     output_folder = "output"
-
-    calculator = VergenceCalculator(data, output_folder)
+    
+    calculator = VergenceCalculator(path2logs, output_folder)
     calculator.process_videos()
 
     plot_all_csv_in_dir(output_folder)
 
-
 if __name__ == "__main__":
-    # main(sys.argv[1])
-    path2logs = "data"
-    print("path2logs= ", path2logs)
-    # print('path2logs= ', path2logs)
+    # Check if a command-line argument is provided
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <path_to_logs>")
+        sys.exit(1)
+
+    path2logs = sys.argv[1]  # Get the path from command-line argument
     main(path2logs)
